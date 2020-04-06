c0959a4a77da97933e2e468196e3c4d5e964eb82
suztsu <suzuki898@gmail.com>
2020-04-06 23:57:03 +0900

    Pythonを別ターミナルで実行するように変更

--- folocommandar/julius.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-/usr/bin/python3 /home/pi/dev/labo/folocommandar/commandar.py
--- folocommandar/run.sh
+++ folocommandar/run.sh
@@ -2,4 +2,4 @@
 
 julius -C ~/julius/dict-kit/dictation-kit-4.5/command.jconf -module
 sleep 5
-/usr/bin/python3 /home/pi/dev/labo/folocommandar/commandar.py
+gnome-terminal --tab --active -- /usr/bin/python3 /home/pi/dev/labo/folocommandar/commandar.py
